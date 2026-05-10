7ae5c6593f1847bde07de026305a31b7ce52f4a1
LNPoly <paula.dominguez2205@gmail.com>
2026-05-10 14:36:13 -0300

    prueba clase inicializador

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # OCR - Deteccion de texto en imagenes 
 
 #acá se ejecuta el programa, se llama a las funciones y se muestra la imagen resultante
+class ProcesorImg:
+    def __init__(self):
+        pass
+
+    def procesar_imagen(self, imagen):
+        # aqui va la logica para procesar la imagen, por ejemplo, eliminar ruido, mejorar contraste, etc.
+        return imagen
